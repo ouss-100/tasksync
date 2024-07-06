86719cde9748b4f9a15bb29ad52366af1ec52867
oussama moussa <moussaoussama63@gmail.com>
2024-07-06 03:12:35 +0100

    delete pre function

--- lib/controller/onboarding_controller.dart
+++ lib/controller/onboarding_controller.dart
@@ -4,19 +4,16 @@ import 'package:tasksync/data/datasource/static/static.dart';
 
 abstract class OnboardingController extends GetxController {
   next();
-  prev();
   onPageChanged(int index);
 }
 
 class OnboardingControllerImpl extends OnboardingController {
   late PageController pageController;
   int currentIndex = 0;
-  RxInt c = 0.obs;
 
   @override
   next() {
     currentIndex++;
-    c.value = currentIndex;
 
     if (currentIndex > onBoardingData.length - 1) {
       Get.offAllNamed('/login');
@@ -27,14 +24,6 @@ class OnboardingControllerImpl extends OnboardingController {
   }
 
   @override
-  prev() {
-    currentIndex--;
-    c.value = currentIndex;
-    pageController.animateToPage(currentIndex,
-        duration: const Duration(milliseconds: 400), curve: Curves.easeIn);
-    currentIndex > 0 ? currentIndex-- : currentIndex = 0;
-  }
-
   @override
   void onInit() {
     pageController = PageController();
